33645eb542a0a9d6f5263499f968e9e75133fb43
jcy_1995 <541866376@qq.com>
2020-03-22 00:05:56 +0800

    算法-最长回文子串(下)

--- LeetCode/src/main/java/com/android/leetcode/LongestPalindrome.java
+++ LeetCode/src/main/java/com/android/leetcode/LongestPalindrome.java
@@ -11,6 +11,8 @@ public class LongestPalindrome {
      * 在动态规划的思想中，总是希望把问题划分成相关联的子问题；然后从最基本的子问题出发来推导较大的子问题，直到所有的子问题都解决。
      * 假设字符串s的长度为length，建立一个length*length的矩阵dp。令 dp[i][j] 表示 S[i] 至 S[j] 所表示的子串是否是回文子串。
      *
+     * 直观的动态规划解法，我们首先初始化一字母和二字母的回文，然后找到所有三字母回文，并依此类推…
+     *
      * 当 i == j，dp[i][j] 是回文子串（单字符都是回文子串）；
      * 当 j - i < 3，只要 S[i] == S[j]，则 dp[i][j] 是回文子串（如"aa"，“aba”），否则不是；
      * 当 j - i >= 3，如果 S[i] == S[j] && dp[i+1][j-1] ，则 dp[i][j] 是回文子串，否则不是 。
@@ -19,23 +21,28 @@ public class LongestPalindrome {
      *
      * abaaba
      *
-     * 作者：https://blog.csdn.net/daidaineteasy/article/details/86238047
+     * 作者：仿写，https://blog.csdn.net/daidaineteasy/article/details/86238047
      */
     public String longestPalindromeStudy(String s) {
         if (s == null || s.length() == 0) return "";
-        int maxLength = 0;
+        int max = 0;
         String maxSubString = "";
         boolean[][] dp = new boolean[s.length()][s.length()];
-        for (int j = 0; j < s.length(); j++) {
-            for (int i = 0; i <= j; i++) {
-                dp[i][j] = s.charAt(i) == s.charAt(j) && ((j - i < 3) || (dp[i + 1][j - 1]));
-                if (dp[i][j] && (j - i + 1 > maxLength)) {
-                    maxLength = j - i + 1;
-                    maxSubString = s.substring(i, j + 1);
+        // i随着j改变，j永远也不会大于i，判断i和j之间字串是否为回文串
+        for (int i = 0; i < s.length(); i++) {
+            // j <= i，原因：在i到了更大的位置，需要保证之前位置的所有情况的字串都已被判断过是否为回文串，已被同之后的位置进行判断
+            for (int j = 0; j <= i; j++) {
+                // 若i - j < 3，只需要判断i和j是否相等，而大于3，则需要判断i和j中间的串是否为回文串，例如：abccba。
+                // 若此时i = 5为最后一个a，j为第一个a(i永远比j大或者等于j)，那么此时就需要判断bccb是否为回文字串，
+                // 而判断bccb是否为回文字串，则需要判断第二位b和第五位b是否相等，然后第三位和第四位是否为回文字串，即第三位是否等于第四位。
+                // 而第三位和第四位是否相等是在i = 3时判断的，而bccb是否为回文字串是在i = 4时判断的，并且用到了i = 3时的结果，最后i = 5的abccba用到了i = 4的结果。
+                dp[j][i] = (s.charAt(i) == s.charAt(j)) && ((i - j < 3) || dp[j + 1][i - 1]);
+                if (dp[j][i] && (i - j + 1) > max) {
+                    max = i - j + 1;
+                    maxSubString = s.substring(j, i + 1);
                 }
             }
         }
-        System.out.println(maxLength);
         return maxSubString;
     }
 
@@ -49,36 +56,36 @@ public class LongestPalindrome {
      * 你可能会问，为什么会是 2n - 12n−1 个，而不是 nn 个中心？
      * 原因在于所含字母数为偶数的回文的中心可以处于两字母之间（例如 \textrm{“abba”}“abba”的中心在两个 \textrm{‘b’}‘b’ 之间）。
      *
-     * @param s
+     * @param str
      * @return
-     * 作者：LeetCode
-     * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode/
-     * 来源：力扣（LeetCode）
-     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * 作者：仿写，https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode/
      */
-    public String longestPalindromeStudy2(String s) {
-        if (s == null || s.length() < 1) return "";
+    private String longestPalindromeStudy2(String str) {
+        if (str == null || str.length() == 0) return "";
         int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        for (int i = 0; i < str.length(); i++) {
+            int len1 = expansionCenter(str, i, i);
+            int len2 = expansionCenter(str, i, i + 1);
+            int max = Math.max(len1, len2);
+            if (max > end - start) {
+                start = i - ((max - 1) >> 1);
+                end = i + (max >> 1);
             }
         }
-        return s.substring(start, end + 1);
+
+        return str.substring(start, end + 1);
     }
 
-    private int expandAroundCenter(String s, int left, int right) {
-        int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
+    private int expansionCenter(String s, int left, int right) {
+        // 由于left和right都是字符串中间字符下标，中心扩散就要往两侧扩散，因此left--、right++，还要保证不越界
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        return R - L - 1;
+        // -1 的原因是：由于在循环结束前，无论left和right位置字符是否相等或者已超边界，此时left都已--而right都已++，又由于下标从0开始，因此只需要 -1即可
+        return right - left - 1;
     }
+
 
     /**
      * Manacher 算法
