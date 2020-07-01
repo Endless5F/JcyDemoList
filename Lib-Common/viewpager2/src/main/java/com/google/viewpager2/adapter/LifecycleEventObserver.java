19a6a60097ed74308f7bad5c8e819cd2afc1da2a
Endless5F <541866376@qq.com>
2020-07-01 13:47:05 +0800

    新增viewpager2

---
+++ Lib-Common/viewpager2/src/main/java/com/google/viewpager2/adapter/LifecycleEventObserver.java
@@ -0,0 +1,16 @@
+package com.google.viewpager2.adapter;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
+import android.support.annotation.NonNull;
+
+public interface LifecycleEventObserver extends LifecycleObserver {
+    /**
+     * Called when a state transition event happens.
+     *
+     * @param source The source of the event
+     * @param event The event
+     */
+    void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event);
+}
---
+++ Lib-Common/viewpager2/src/main/java/com/google/viewpager2/adapter/StatefulAdapter.java
@@ -0,0 +1,13 @@
+package com.google.viewpager2.adapter;
+
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+
+public interface StatefulAdapter {
+    /** Saves adapter state */
+    @NonNull
+    Parcelable saveState();
+
+    /** Restores adapter state */
+    void restoreState(@NonNull Parcelable savedState);
+}
