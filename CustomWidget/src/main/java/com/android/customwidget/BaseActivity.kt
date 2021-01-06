7664615987bfdb54d11763526b2adb568db980b8
Endless5F <541866376@qq.com>
2021-01-06 18:52:32 +0800

    沉浸式状态栏

--- CustomWidget/src/main/java/com/android/customwidget/BaseActivity.kt
+++ CustomWidget/src/main/java/com/android/customwidget/BaseActivity.kt
@@ -1,16 +1,25 @@
 package com.android.customwidget
 
-import android.annotation.SuppressLint
+import android.graphics.Color
 import android.text.TextUtils
+import android.view.View
+import android.view.WindowManager
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.android.customwidget.kotlin.widget.floatwindow.FloatBackManager
 
-@SuppressLint("Registered")
 open class BaseActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        if (isImmersiveStatusBar()) {
+            val window = window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+            val uiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or if (isLightStatusBar()) View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR else View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.decorView.systemUiVisibility = uiVisibility
+        }
         if (!TextUtils.isEmpty(FloatBackManager.instance?.schemeBackFrom)) {
             FloatBackManager.instance?.addFloatBackView(this, window.decorView as FrameLayout)
         }
@@ -20,6 +29,14 @@ open class BaseActivity : AppCompatActivity() {
         super.onPause()
         // 触发
         FloatBackManager.instance?.schemeBackFrom = touchFloatBackEvent()
+    }
+
+    open fun isImmersiveStatusBar(): Boolean {
+        return true
+    }
+
+    open fun isLightStatusBar(): Boolean {
+        return true
     }
 
     open fun touchFloatBackEvent(): String? {
